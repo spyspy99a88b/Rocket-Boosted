7774fd4410e5b6da3a9648a5bdef60d715e07240
spyspy99a88b <8270952@qq.com>
2020-03-18 11:02:34 -0400

    create oscillator of movement and rotation

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+    [SerializeField] float movementOffset = 0.5f;
+
+    float movementFactor; // 0 for not moved, 1 for fully moved.
+    Vector3 startingPos;
+
+    // Use this for initialization
+	void Start () {
+        startingPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (period <= Mathf.Epsilon) { return; } // protect against period is zero
+        float cycles = Time.time / period; 
+
+        const float tau = Mathf.PI * 2f; 
+        float rawSinWave = Mathf.Sin(cycles * tau); // goes from -1 to +1
+
+        movementFactor = rawSinWave / 2f + movementOffset; //from [0,1]
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
+	}
+}
---
+++ Assets/Oscillator2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[DisallowMultipleComponent]
+public class Oscillator2 : MonoBehaviour
+{
+    
+	void Start () {
+        
+	}
+
+	void Update () {
+        transform.Rotate(Vector3.up,Time.deltaTime * 20,Space.World);
+	}
+}
