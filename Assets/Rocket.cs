eb8b114c5d3ce6f2b0244bcfd76d72782cd11413
spyspy99a88b <8270952@qq.com>
2020-03-17 20:07:42 -0400

    voice,rotation,launch added

---
+++ Assets/Rocket.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        ProcessInput();
+    }
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if(!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);  
+        }
+    }
+}
